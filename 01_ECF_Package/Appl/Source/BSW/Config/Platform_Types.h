9e50bf96d033e6935c742a359885f17a19475a01
mada61988 <mada61988@gmail.com>
2023-11-14 14:26:34 +0200

    first init commit

---
+++ 01_ECF_Package/Appl/Source/BSW/Config/Platform_Types.h
@@ -0,0 +1,21 @@
+/*
+ *         Name :STD_TYPS.h
+ *
+ *          Date: Nov 20, 2022
+ *        Author: MohamedAymanShokry
+ */
+
+#ifndef CONF_STD_TYPS_H_
+#define CONF_STD_TYPS_H_
+
+typedef unsigned short int      uint16;
+typedef unsigned long int       uint32;
+typedef unsigned char           uint8;
+typedef signed char             sint8;
+
+#define E_OK                       0 
+#define E_NOK                      1
+#define NULL                      255
+
+
+#endif /* CONF_STD_TYPS_H_ */
