003c194653b28680ae7c160c272725ff08b684f8
mada61988 <mada61988@gmail.com>
2023-11-14 19:18:11 +0200

    Build python script

--- 00_EUF_Application/Build/ATGD_Build.py
+++ 00_EUF_Application/Build/ATGD_Build.py
@@ -2,14 +2,65 @@
 import shutil
 import os
 
-Dest="./"
-
-TDSHeaderFilePath="../TDS/inc/TDS.h"
-TDSWrapperFilePath="../TDS/inc/TDS_Wrp.h"
+#Stds
+destination_folder="./"
 
 
-# Copy
-shutil.copy(TDSHeaderFilePath , Dest)
-#shutil.copy(TDSWrapperFilePath , "./")
+#DirectoryPath
+	#TDS
+TDSIncFilePath="../TDS/inc/"
+TDSSrcFilePath="../TDS/src/"
+	#GDS
+GDSIncFilePath="../GDS/inc/"
+GDSSrcFilePath="../GDS/src/"
+	#DIO
+DIOFilePath="../../01_ECF_Package/Appl/Source/BSW/DIO/"
+	#LM35
+LM35IncFilePath="../../01_ECF_Package/Appl/Source/BSW/LM35/inc/"
+LM35SrcFilePath="../../01_ECF_Package/Appl/Source/BSW/LM35/src/"
+	#MQ2
+MQ2IncFilePath="../../01_ECF_Package/Appl/Source/BSW/MQ2/inc/"
+MQ2SrcFilePath="../../01_ECF_Package/Appl/Source/BSW/MQ2/src/"
+	#OS
+OSFilePath="../../01_ECF_Package/Appl/Source/BSW/OS/"
+	#RTE
+RTEFilePath="../../01_ECF_Package/Appl/Source/BSW/RTE/"
+	#CONFIG
+ConfigFilePath="../../01_ECF_Package/Appl/Source/BSW/Config/"
 
-print("Hello World")
+
+# Function Implementation
+def copyDirectoryContent(source_folder):
+	# fetch all files
+	for file_name in os.listdir(source_folder):
+		# construct full file path
+		source=source_folder+file_name
+		destination=destination_folder+file_name
+		# copy only files
+		if os.path.isfile(source):
+			shutil.copy(source, destination)
+			print('copied', file_name)
+
+
+
+
+copyDirectoryContent(TDSIncFilePath)
+copyDirectoryContent(TDSSrcFilePath)
+
+copyDirectoryContent(GDSIncFilePath)
+copyDirectoryContent(GDSSrcFilePath)
+
+copyDirectoryContent(DIOFilePath)
+#copyDirectoryContent(GDSSrcFilePath)
+
+copyDirectoryContent(LM35IncFilePath)
+copyDirectoryContent(LM35SrcFilePath)
+
+copyDirectoryContent(MQ2IncFilePath)
+copyDirectoryContent(MQ2SrcFilePath)
+
+copyDirectoryContent(OSFilePath)
+
+copyDirectoryContent(RTEFilePath)
+
+copyDirectoryContent(ConfigFilePath)
